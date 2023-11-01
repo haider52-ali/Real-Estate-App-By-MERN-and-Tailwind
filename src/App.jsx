93fad0ab053eeee52897ab72d29db574427b0adb
Ali Haider <alihaider.logics@gmail.com>
2023-11-01 06:57:29 +0500

    install react and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <h1 className='text-center text-4xl py-4 font-bold underline text-red-600 bg-black'>
+      Hello World!
+    </h1>
+  )
+}
